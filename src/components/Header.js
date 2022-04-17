d3d956ea1d2a05402d989f22233c849b7c0e584b
marleigh <volcana@gmail.com>
2022-04-17 13:03:03 -0700

    initial setup, with start on a header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import Nav from "./Nav";
+
+export default function Header() {
+    return (
+        <header className="header-container">
+            <div>
+                logo
+            </div>
+            <Nav/>
+        </header>
+    )
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,7 @@
+export default function Nav() {
+    return (
+        <div>
+            links here
+        </div>
+    )
+}
