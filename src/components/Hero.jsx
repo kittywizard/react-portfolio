10ea9f918b79d2720c5d1a88cc24df7d0a4ede35
marleigh <volcana@gmail.com>
2022-08-17 15:57:24 -0700

    starting on hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,12 @@
+import {ImPen} from "react-icons/im";
+
 export default function Hero() {
     return (
-        <section>
-            wow 
+        <section className="hero-section">
+            <ImPen />
+            <h1 className="hero-headline">
+                Marleigh Morgan
+            </h1> 
         </section>
     )
 }
